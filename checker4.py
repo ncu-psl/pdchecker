0c92d8a5905dd3127b2a9e502ed300802902f611
Ming-Yang Lu <op8867555@gmail.com>
2020-05-05 20:26:17 +0800

    clean up prototype files.

---
+++ checker4.py
@@ -0,0 +1,348 @@
+import ast
+
+from typing import List
+
+from spec4 import *
+from collections import defaultdict
+
+from functools import partial, partialmethod
+
+from pdb import set_trace
+
+import spec4 as spec
+
+def with_ast(node, ast_node):
+    if type(node) is list:
+        class list_with_ast(list):
+            def __init__(self, *args, **kwargs):
+                list.__init__(*args, **kwargs)
+        node = list_with_ast(node)
+    elif type(node) is str:
+        class str_with_ast(str):
+            def __init__(self, *args, **kwargs):
+                str.__init__(*args, **kwargs)
+        node = str_with_ast(node)
+    setattr(node, 'ast', ast_node)
+    return node
+
+def mark_slice(subs, s):
+    setattr(s, 'lineno', subs.value.end_lineno)
+    setattr(s, 'col_offset', subs.value.end_col_offset)
+    setattr(s, 'end_lineno', subs.end_lineno)
+    setattr(s, 'end_col_offset', subs.end_col_offset)
+
+
+
+class Annotator(ast.NodeTransformer):
+    def visit_Subscript(self, node: ast.Subscript):
+        slice = node.slice
+        if type(slice) is ast.Index:
+            ast.copy_location(node, node.value)
+        return node
+
+class Interpreter:
+    def interprets(self, xs):
+        return [self.interpret(x) for x in xs]
+    def interpret(self, a):
+        if not a:
+            return
+        elif type(a) == ast.Module:
+            body = self.interprets(a.body)
+            return self.Module(a, body)
+        elif type(a) == ast.Expression:
+            return self.Expression(a.body)
+        elif type(a) == ast.ClassDef:
+            body = self.interprets(a.body)
+            return self.ClassDef(a, a.name, a.bases, a.keywords, body, a.decorator_list)
+        elif type(a) == ast.FunctionDef:
+            # body = self.interprets(a.body)
+            return self.FunctionDef(a, a.body)
+        elif type(a) == ast.Expr:
+            return self.interpret(a.value)
+        elif type(a) == ast.Call:
+            f = self.interpret(a.func)
+            args = self.interprets(a.args)
+            kws = self.interprets(a.keywords)
+            return self.Call(a, f, args, kws)
+        elif type(a) == ast.Constant:
+            v = a.value
+            return self.Constant(a, v)
+        elif type(a) == ast.Dict:
+            keys = self.interprets(a.keys)
+            values = self.interprets(a.values)
+            return self.Dict(a, keys, values)
+        elif type(a) == ast.List:
+            elts = self.interprets(a.elts)
+            return self.List(a, elts, a.ctx)
+        elif type(a) == ast.Tuple:
+            # XXX
+            elts = self.interprets(a.elts)
+            return self.List(a, elts, a.ctx)
+        elif type(a) == ast.Name:
+            _id = a.id
+            ctx = a.ctx
+            return self.Name(a, _id, ctx)
+        elif type(a) == ast.keyword:
+            v = self.interpret(a.value)
+            return self.Keyword(a, a.arg, v)
+        elif type(a) == ast.Attribute:
+            v = self.interpret(a.value)
+            return self.Attribute(a, v, a.attr, a.ctx)
+        elif type(a) == ast.Subscript:
+            v = self.interpret(a.value)
+            _slice = self.interpret(a.slice)
+            return self.Subscript(a, v, _slice, a.ctx)
+        elif type(a) == ast.Index:
+            v = self.interpret(a.value)
+            return self.Index(a, v)
+        elif type(a) == ast.Import:
+            return self.Import(a, a.names)
+        elif type(a) == ast.Assign:
+            targets = self.interprets(a.targets)
+            values = self.interpret(a.value)
+            return self.Assign(a, targets, values)
+        elif type(a) == ast.BinOp:
+            left = self.interpret(a.left)
+            right = self.interpret(a.right)
+            return getattr(self, type(a.op).__name__)(a, left, right)
+        elif type(a) == ast.Slice:
+            lower = self.interpret(a.lower)
+            upper = self.interpret(a.upper)
+            step  = self.interpret(a.step)
+            s = self.Slice(a, lower=lower, upper=upper, step=step)
+            return 
+        elif type(a) == ast.ExtSlice:
+            dims = self.interprets(a.dims)
+            return self.ExtSlice(a, dims)
+        elif type(a) == ast.Return:
+            value = self.interpret(a.value)
+            return self.Return(a, value)
+        else:
+            if type(a) is list:
+                if a:
+                    raise Exception(ast.dump(a[0]))
+                else:
+                    raise Exception([])
+            else:
+                raise Exception(ast.dump(a))
+
+
+class Ty(Interpreter):
+    env = {}
+    def Module(self, a, body):
+        return body[-1]
+    def Expression(self, a, body):
+        return self.interpret(body)
+    def Import(self, a, names):
+        for n in a.names:
+            if type(n) == ast.alias:
+                if n.name == 'pandas':
+                    self.env[n.asname] = spec
+    def Name(self, a, _id, ctx):
+        if type(ctx) == ast.Store:
+            return partial(self.env.__setitem__, _id)
+        return self.env[_id]
+    def Assign(self, a, targets, value):
+        if len(targets) > 1:
+            return None
+        return targets[0](value)
+    def Attribute(self, a, value, attr, _ctx):
+        attr = with_ast(attr, a)
+        if not value:
+            raise CheckerNotImplementedError(a, attr)
+        return getattr(value, attr)
+    def Constant(self, a, v):
+        if type(v) is str:
+            return StrLike(v)
+        elif type(v) is int:
+            return IntLike(v)
+        elif type(v) is bool:
+            return Bool(v)
+        else:
+            raise TypeError(f'unsupport constant: {v !r}')
+    def Call(self, a, f, args, kwargs):
+        # TODO: align params here
+        args = [with_ast(n, an) for n, an in zip(args, a.args)]
+        kwargs = [(k, with_ast(n, an)) for (k, n), an in zip(kwargs, a.keywords)]
+        return f(*args, **dict(kwargs))
+    def Keyword(self, a, arg, value):
+        return (arg, value)
+    def Subscript(self, a, v, _slice, _ctx):
+
+        mark_slice(a, a.slice)
+        if type(_ctx) == ast.Store:
+            return partial(v.__setitem__, _slice.val)
+        s = with_ast(_slice, a.slice)
+        if v:
+            return v[s]
+        raise CheckerError('')
+    def Index(self, a, v):
+        return v
+    def Dict(self, a, keys, values):
+        # XXXX
+        return DictLike({k.val:v for k, v in zip(keys, values)})
+    def List(self, a, elts, v):
+        # XXX
+        if elts:
+            return ListLike(elts, elts[0])
+        else:
+            return ListLike([], None)
+    def Add(self, a, left, right):
+        return left + right
+    def Sub(self, a, left, right):
+        return left - right
+    def Mul(self, a, left, right):
+        return left * right
+    def Div(self, a, left, right):
+        return left / right
+    def Slice(self, a, lower, upper, step):
+        return slice(lower, upper, step)
+    def ExtSlice(self, a, dims):
+        return dims
+    def FunctionDef(self, a: ast.FunctionDef, body):
+        ret_type = self.from_annotation(a.returns)
+        arg_type = [self.from_annotation(arg.annotation) for arg in a.args.args]
+        class Func:
+            def __call__(self, *args):
+                for arg, expected in zip(args, arg_type):
+                    if not arg.subtype_of(expected):
+                        raise CheckerTypeError(None, expected, arg)
+                return ret_type
+            def __repr__(self):
+                return f'Func({a.name}: {arg_type} -> {ret_type})'
+        f = Func()
+        self.env[a.name] = f
+        return None
+    def Return(self, *args):
+        return
+    def from_annotation(self, a: ast.Expr):
+        #XXX
+        assert type(a) == ast.Name
+        e = a.id
+        if e == 'int':
+            return IntLike(None)
+        elif e == 'str':
+            return StrLike(None)
+        elif e == 'float':
+            return FloatLike()
+        else:
+            raise CheckerNotImplementedError(a, a)
+
+class TyLog(Ty):
+    def __init__(self):
+        self.srcmap = {}
+    def __getattribute__(self, attr):
+        orig = Ty.__getattribute__(self, attr)
+        if not hasattr(orig, '__call__') or not orig.__name__[0].isupper():
+            return orig
+        if attr != 'Name':
+            def f(a, *args, **kwargs):
+                res = orig(a, *args, **kwargs)
+                self.srcmap[a] = res
+                return res
+        else:
+            def f(a, _id, ctx):
+                res = orig(a, _id, ctx)
+                if type(ctx) != ast.Store:
+                    self.srcmap[a] = res
+                return res
+        f.__name__ = attr
+        return f
+
+
+
+class TyError(TyLog):
+    def __init__(self):
+        TyLog.__init__(self)
+        self.errors = []
+
+
+
+    def __getattribute__(self, attr):
+        orig = TyLog.__getattribute__(self, attr)
+        if not hasattr(orig, '__call__') or not orig.__name__[0].isupper():
+            return orig
+        def f(a, *args, **kwargs):
+            try:
+                res = orig(a, *args, **kwargs)
+                return res
+            except CheckerError as e:
+                def info(e, a, attr):
+                    if hasattr(e, 'ast') and hasattr(e.ast, attr):
+                        return getattr(e.ast, attr)
+                    return getattr(a, attr)
+                self.errors.append({
+                    'node': attr,
+                    'lineno':         info(e, a, 'lineno'),
+                    'col_offset':     info(e, a, 'col_offset'),
+                    'end_lineno':     info(e, a, 'end_lineno'),
+                    'end_col_offset': info(e, a, 'end_col_offset'),
+                    'error': e
+                    })
+        f.__name__ = attr
+        return f
+
+class Sp(Interpreter):
+    env = {}
+    def Module(self, a, body):
+        pass
+    def Name(self, a, _id, ctx):
+        pass
+    def ClassDef(self, a, name, bases, keywords, body, decorator_list):
+        self.env[name] = defaultdict(list)
+        for k, v in body:
+            self.env[name][k].append(v)
+    def FunctionDef(self, a: ast.FunctionDef, body):
+        return (a.name, match)
+    def Constant(self, a, v):
+        return
+
+
+# sp = Sp()
+# code = '''
+# class D:
+#     def f(x: int) -> int: ...
+# '''
+# sp.interpret(ast.parse(code))
+# for k,v in Sp.env.items():
+#     for f, bs in v.items():
+#         for b in bs:
+#             print(k, f, [ast.dump(x) for x in b])
+
+class Literal:
+    def __getitem__(self, value):
+        return LiteralType(value)
+
+def check(code):
+    itpr = TyError()
+    # itpr.env['df1'] = DataFrame(_index=int, _columns={'x': int})
+    # itpr.env['sum']     = Func(IntLike, IntLike)
+    itpr.env['Literal'] = Literal()
+    itpr.interpret(ast.parse(code))
+    return itpr
+
+code = '''
+import pandas as pd
+df = pd.read_csv('./test.csv')
+def some_transform(x: float) -> int:
+    return x ** 2
+df['b'] = df['b'].apply(some_transform)
+df1 = pd.DataFrame([[1,2,'a'], [2, 2, 'a']], columns=['a','d','c'])
+df.merge(df1, on='a')[['c']]
+df.a
+'''
+
+'''
+'''
+
+itpr = TyError()
+itpr.env['Literal'] = Literal()
+a = ast.parse(code)
+res = itpr.interpret(a)
+print(res)
+print(itpr.errors)
+
+# print(t)
+# print(itpr.env)
+# print(itpr.srcmap)
+
---
+++ spec4.py
@@ -0,0 +1,396 @@
+from typing import List, Tuple, Callable, Dict, Optional, Type, Any, Union, Generic, TypeVar, get_args, get_origin
+from dataclasses import dataclass, field
+
+class CheckerError(Exception):
+    def __init__(self, message, ast=None):
+        self.message = message
+        self.ast = ast
+    def __str__(self):
+        return self.message
+
+class CheckerTypeError(CheckerError):
+    def __init__(self, ast=None, expected=None, actual=None):
+        self.ast = ast
+        self.message = f'TypeError: expected {expected!r} but got {actual!r}'
+
+class CheckerNotImplementedError(CheckerError):
+    def __init__(self, ast=None, obj=None):
+        self.obj = obj
+        self.ast = ast
+        self.message = f'Not implemented for {obj !r}.'
+
+class CheckerIndexError(CheckerError):
+    def __init__(self, index, df=None, ast=None):
+        self.index = index
+        self.ast = ast
+        self.message = f'Index {index !r} not found.'
+
+
+class CheckerLackOfInfo(CheckerError):
+    def __init__(self, ast):
+        self.message = 'Lack of column types.'
+        self.ast = ast
+
+class CheckerParamError(CheckerError):
+    def __init__(self, p, ps, ast=None):
+        self.mesage = f'Parameter {p !r} is not in {ps !r}'
+        self.ast = ast
+
+def ensure_labels(df, col):
+    missing = []
+    for label in col:
+        if label not in df.columns:
+            missing.append(label)
+    if missing:
+        raise CheckerIndexError(missing, df)
+
+def from_dtype(dt):
+    if dt.kind == 'i':
+        return IntLike(None)
+    elif dt.kind == 'f':
+        return FloatLike()
+    elif dt.kind == 'O':
+        # XXX
+        return StrLike(None)
+    else:
+        raise CheckerNotImplementedError(obj=dt)
+
+
+def read_csv(fp):
+    import pandas as pd
+    df = pd.read_csv(fp.val)
+    return DataFrame(_index=df.index.dtype,
+                     _columns={k: from_dtype(v)
+                                  for k, v in df.dtypes.to_dict().items()})
+
+class Type:
+    def subtype_of(self, other):
+        return other.subtype(self)
+    def subtype(self, other):
+        return NotImplemented
+    def binop(self, other):
+        return NotImplemented
+    def rbinop(self, other):
+        return NotImplemented
+
+    def __add__(self, other):  return self.binop(other)
+    def __sub__(self, other):  return self.binop(other)
+    def __mul__(self, other):  return self.binop(other)
+    def __div__(self, other):  return self.binop(other)
+    def __radd__(self, other): return self.rbinop(other)
+    def __rsub__(self, other): return self.rbinop(other)
+    def __rmul__(self, other): return self.rbinop(other)
+    def __rdiv__(self, other): return self.rbinop(other)
+
+
+class NoneType(Type):
+    __single = None
+
+    def __new__(clz):
+        if not NoneType.__single:
+            NoneType.__single = object.__new__(clz)
+        return NoneType.__single
+
+
+@dataclass
+class LiteralType(Type):
+    kinds: List[Type]
+    def __init__(self, vs):
+        if type(vs) is ListLike:
+            self.kinds = vs.val
+        else:
+            self.kinds = vs
+    def subtype(other):
+        return any(other.val == k.val for k in kinds)
+
+@dataclass
+class Bool(Type):
+    val: bool
+    def __bool__(self):
+        return bool(self.val)
+
+
+@dataclass
+class FloatLike(Type):
+    pass
+
+@dataclass
+class IntLike(Type):
+    val: int
+    def __bool__(self):
+        return bool(self.value)
+    def empty(self):
+        return IntLike(None)
+    def subtype(self, other):
+        if type(other) is IntLike:
+            return True
+        return False
+    def binop(self, other):
+        if type(other) is IntLike:
+            return self.empty()
+        else:
+            return NotImplemented
+
+@dataclass
+class StrLike(Type):
+    val: str
+
+@dataclass
+class ListLike(Type):
+    val: Any
+    typ: Type
+
+
+@dataclass
+class DictLike(Type):
+    val: Any
+
+
+@dataclass
+class Func(Type):
+    arg: Any
+    ret: Any
+    def __call__(self, arg):
+        if arg != self.arg:
+            raise CheckerError()
+        return self.ret
+
+@dataclass
+class Series(Type):
+    index: Type = None
+    value: Type = None
+    def __init__(self, data = None, index=None, *, _index=None, _value=None):
+        self.index = _index
+        self.value = _value
+        if type(data) is ListLike:
+            self.value = data.typ
+
+    def apply(self, func):
+        return Series(_index=self.index, _value=func(self.value))
+
+    def binop(self, other):
+        return Series(_index=self.index, _value=self.value.binop(other))
+
+    __add__ = binop
+    __sub__ = binop
+    __mul__ = binop
+    __truediv__ = binop
+
+@dataclass
+class LocIndexerFrame(Type):
+    df: 'DataFrame'
+
+    def __getitem__(self, idx):
+        if type(idx) is not list:
+            return #XXX
+
+        col = idx[1]
+        if type(col) is StrLike:
+            if col.val in self.df.columns:
+                return Series(_index=self.df.index, _value=self.df.columns.get(col.val))
+            else:
+                raise CheckerIndexError(index=col.val, df=self.df, ast=idx.ast)
+        elif type(col) is ListLike:
+            res = {}
+            missing = []
+            for label in col.val:
+                if label.val in self.df.columns:
+                    res[label.val] = self.df.columns[label.val]
+                else:
+                    missing.append(label)
+            if missing:
+                raise CheckerIndexError(index=missing)
+            return DataFrame(_index=self.df.index, _columns=res)
+        elif type(col) is slice:
+            return self
+        else:
+            raise CheckerNotImplementedError(col)
+
+
+@dataclass
+class DataFrame(Type):
+    index: Type = None
+    columns: Dict[str, Type] = field(default_factory=dict)
+
+
+    def __getattr__(self, attr):
+        if attr in self.columns:
+                return Series(_index=self.index, _value=self.columns.get(attr))
+        raise CheckerNotImplementedError(attr.ast, attr)
+
+    def __init__(self, data=None, index=None, columns=None, *, _index=None, _columns=None):
+        self.index = _index
+        self.columns = _columns
+        if type(data) is ListLike and type(data.typ) is ListLike:
+            if not index:
+                self.index = IntLike(None)
+            if not columns:
+                self.columns = {i:x for i, x in enumerate(data.typ.val)}
+            else:
+                self.columns = {i.val:x for i, x in zip(columns.val, data.typ.val)}
+
+
+
+    def __getitem__(self, idx):
+        if type(idx) is StrLike:
+            if idx.val in self.columns:
+                return Series(_index=self.index, _value=self.columns.get(idx.val))
+            else:
+                raise CheckerIndexError(index=idx.val, df=self, ast=idx.ast)
+        elif type(idx) is ListLike:
+            res = {}
+            missing = []
+            for label in idx.val:
+                if label.val in self.columns:
+                    res[label.val] = self.columns[label.val]
+                else:
+                    missing.append(label.val)
+            if missing:
+                raise CheckerIndexError(index=missing, ast=idx.ast)
+            return DataFrame(_index=self.index, _columns=res)
+        elif type(idx) is slice:
+            return self
+        else:
+            raise CheckerNotImplementedError(idx.ast, idx)
+
+    def __setitem__(self, idx, value):
+        new = self.assign(**{idx: value})
+        self.columns = new.columns
+        self.index = new.index
+
+
+    @property
+    def loc(self):
+        return LocIndexerFrame(self)
+
+    def assign(self, **kwargs: Dict[str, Type]):
+        new_cols = {}
+        for k, v in kwargs.items():
+            if isinstance(v, Series):
+                new_cols[k] = v.value
+            elif type(v) is Func:
+                ret_type = v.ret
+                if isinstance(ret_type, Series):
+                    new_cols[k] = ret_type.value
+                else:
+                    raise CheckerError('Callable not returning a Series')
+            elif type(v) is ListLike:
+                new_cols[k] = v.value
+            # else:
+            #     raise Exception(f'Not type checked: {v!r}')
+        return DataFrame(_index=self.index, _columns={**self.columns, **new_cols})
+
+    def count(self, axis=None):
+        if axis in [None, 0, 'index']:
+            return Series(_index=str, _value=int)
+        elif axis in [1, 'columns']:
+            return Series(_index=self.index, _value=int)
+
+    def describe(self):
+        return DataFrame()
+
+    def merge(self, other, on=None, how=None):
+        possible_how = ['inner', 'left', 'right', 'outer']
+        if how and how.val not in possible_how:
+            raise CheckerParamError(how.val, possible_how)
+        if not isinstance(other, DataFrame):
+            raise CheckerError('other is not a DataFrame')
+        other: DataFrame = other
+        if type(on) is not ListLike:
+            on = ListLike([on], on)
+        on_labels = [lbl.val for lbl in on.val]
+        ensure_labels(self, on_labels)
+        ensure_labels(other, on_labels)
+
+        left_fields  = [ self.columns[lbl] for lbl in on_labels]
+        right_fields = [other.columns[lbl] for lbl in on_labels]
+
+        if not all(t1.subtype(t2) for t1, t2 in zip(left_fields, right_fields)):
+            raise CheckerError('type mismatch')
+
+        overlapped = set(self.columns.keys()).intersection(set(other.columns.keys())) - set(on_labels)
+        new = {}
+
+        for lbl, typ in self.columns.items():
+            if lbl in overlapped:
+                lbl += '_x'
+            new[lbl] = typ
+
+        for lbl, typ in other.columns.items():
+            if lbl in overlapped:
+                lbl += '_y'
+            new[lbl] = typ
+
+        for lbl in on_labels:
+            new[lbl] = self.columns[lbl]
+
+        return DataFrame(_index=self.index, _columns=new)
+
+    def groupby(self, by=None):
+        key = None
+        if type(by) is StrLike and by.val in self.columns:
+            key = [by.val]
+        elif type(by) is ListLike and by.typ is StrLike:
+            if not all(lbl.val in self.columns for lbl in by.val):
+                raise CheckerError('key not found')
+            else:
+                key = [lbl.val for lbl in by.val]
+        if key:
+            return DataFrameGroupBy(key, self)
+        raise CheckerError('Not type checked')
+
+
+    def drop_duplicates(self, subset=None, keep=None):
+        missing = []
+        for label in subset.val:
+            if label.val not in self.columns:
+                missing.append(label.val)
+        if missing:
+            raise CheckerIndexError(index=missing)
+        return self
+
+    def sort_values(self, by, axis=None, ascending=None, inplace=None, kind=None, na_position=None, ignore_index=None):
+        if type(by) is not ListLike:
+            by = ListLike([by], by)
+        missing = []
+        for label in by.val:
+            if label.val not in self.columns:
+                missing.append(label.val)
+        if missing:
+            raise CheckerIndexError(index=missing)
+
+        if inplace:
+            return None
+        
+        return self
+
+
+    def pivot(self, index, columns, values):
+        # TODO multiple column/values
+        idx    = self.columns[index.val]
+        col    = self.columns[columns.val]
+        value  = self.columns[values.val]
+        if type(col) is LiteralType:
+            new_labels = col.kinds
+            new_col    = {l.val:value for l in new_labels}
+            return DataFrame(_index=idx, _columns=new_col)
+        else:
+            # try ask a LiteralType
+            raise CheckerNotImplementedError()
+
+    def hint_cast(self, **kwargs):
+        return DataFrame(_index=self.index, _columns={**self.columns, **kwargs})
+
+@dataclass
+class DataFrameGroupBy:
+    key: List[str] = None
+    df: DataFrame = None
+
+    def agg(self, func, axis=0):
+        if type(func) is not Func and not hasattr(func, '__call__'):
+            raise CheckerError('not a function')
+
+        return DataFrame(_index=tuple(self.df.columns[k] for k in self.key),
+                        _columns={k: func(v) for k, v in self.df.columns.items() if k not in self.key})
+
+        raise CheckerError('Not Type checked')
